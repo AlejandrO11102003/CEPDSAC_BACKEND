99edfb76e14bfbbdec2cf4f0e64be363e088cdc5
AlejandrO11102003 <u22239328@utp.edu.pe>
2025-12-01 09:50:54 -0500

    varios

--- src/main/java/com/example/cepsacbackend/repository/TestimonioRepository.java
+++ src/main/java/com/example/cepsacbackend/repository/TestimonioRepository.java
@@ -2,10 +2,13 @@ package com.example.cepsacbackend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
 import com.example.cepsacbackend.model.Testimonio;
 
 @Repository
 public interface TestimonioRepository extends JpaRepository<Testimonio, Integer> {
-    
+    @Query("SELECT t FROM Testimonio t JOIN FETCH t.idUsuario")
+    List<Testimonio> findAll();
 }
